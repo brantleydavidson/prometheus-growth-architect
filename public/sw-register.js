37c788dfc9f6bcc803776dd9e5b88088667541d7
Brantley Davidson <brantley@teamprometheus.co>
2025-05-20 16:56:32 -0500

    Improve prerendering SEO injection, update initial pillar logic, adjust OG URL, log wording, add scoreCalculation test

--- public/sw-register.js
+++ public/sw-register.js
@@ -2,7 +2,7 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js')
       .then(() => {
-        console.log('ServiceWorker registration successful');
+        console.log('Service worker registered successfully');
       })
       .catch((err) => {
         console.log('ServiceWorker registration failed: ', err);
